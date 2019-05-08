1e7e270b45873b416b87c7ccd7eab3602ab05933
s110619045 <49371664+s110619045@users.noreply.github.com>
2019-05-08 11:33:57 +0800

    Add files via upload

---
+++ sketch_may08a.ino
@@ -0,0 +1,23 @@
+const int inputBtn=2;
+const int ledPin=13;
+int btnInput=0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(inputBtn,INPUT);
+  pinMode(ledPin,OUTPUT);
+  digitalWrite(inputBtn,HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  btnInput=digitalRead(inputBtn);
+  if(btnInput == HIGH){
+    digitalWrite(ledPin,HIGH);
+    }
+    else{
+     digitalWrite(ledPin,LOW); 
+      }
+}
+
+
